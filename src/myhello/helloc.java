7a8e78966c927ec31b13a10a384492a7ffaef3eb
merishigarg06 <merishigarg06@gmail.com>
2021-05-17 21:07:14 +0530

    First Java Commit

---
+++ src/myhello/helloc.java
@@ -0,0 +1,14 @@
+package myhello;
+import java.util.Scanner;
+public class helloc 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your name: ");
+		String a=sc.nextLine();
+		System.out.println("Your name is: "+a); // TODO Auto-generated method stub
+
+	}
+
+}
